91443462201918ac7d7ef2e9bab49fe79e5b41da
Joshua Manley <32532286+verymanley@users.noreply.github.com>
2018-01-19 00:49:47 +0900

    Create osx-set-defaults.sh

---
+++ global/osx-set-defaults.sh
@@ -0,0 +1,20 @@
+
+#!/bin/bash
+
+###############################################################################
+# Finder
+###############################################################################
+
+# Always open everything in Finder's column view.
+defaults write com.apple.Finder FXPreferredViewStyle Nlsv
+
+# Disable the warning before emptying the Trash
+defaults write com.apple.finder WarnOnEmptyTrash -bool false
+
+###############################################################################
+# Screen
+###############################################################################
+
+# Require password immediately after sleep or screen saver.
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
