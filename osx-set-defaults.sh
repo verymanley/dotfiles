d44dd5394663994837d21033a7fbbaba1d2ebd0a
Joshua Manley <32532286+verymanley@users.noreply.github.com>
2018-01-26 14:35:05 +0900

    Update osx-set-defaults.sh

--- osx-set-defaults.sh
+++ osx-set-defaults.sh
@@ -1,19 +1,26 @@
--#!/bin/bash
--
--###############################################################################
--# Finder
--###############################################################################
--
--# Always open everything in Finder's column view.
--defaults write com.apple.Finder FXPreferredViewStyle Nlsv
--
--# Disable the warning before emptying the Trash
--defaults write com.apple.finder WarnOnEmptyTrash -bool false
--
--###############################################################################
--# Screen
--###############################################################################
--
--# Require password immediately after sleep or screen saver.
--defaults write com.apple.screensaver askForPassword -int 1
--defaults write com.apple.screensaver askForPasswordDelay -int 0
+#!/bin/bash
+
+###############################################################################
+# General UI/UX                                                               #
+###############################################################################
+
+# Disable the “Are you sure you want to open this application?” dialog
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+
+###############################################################################
+# Finder                                                                      #
+###############################################################################
+
+# Always open everything in Finder's column view.
+defaults write com.apple.Finder FXPreferredViewStyle Nlsv
+
+# Disable the warning before emptying the Trash
+defaults write com.apple.finder WarnOnEmptyTrash -bool false
+
+###############################################################################
+# Screen                                                                      #
+###############################################################################
+
+# Require password immediately after sleep or screen saver.
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
