6843cedbb7fea0845be2d4e73ae507ea776a7a60
Joshua Manley <32532286+verymanley@users.noreply.github.com>
2018-02-01 12:31:34 +0900

    Update osx-set-defaults.sh

--- osx-set-defaults.sh
+++ osx-set-defaults.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-###############################################################################
-# General UI/UX                                                               #
+# General UI/UX                                                               
 ###############################################################################
 
 # Disable the “Are you sure you want to open this application?” dialog
@@ -10,8 +9,7 @@ defaults write com.apple.LaunchServices LSQuarantine -bool false
 # Reveal IP address, hostname, OS version, etc. when clicking the clock in the login window
 sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
 
-###############################################################################
-# Finder                                                                      #
+# Finder                                                                      
 ###############################################################################
 
 # Always open everything in Finder's column view.
@@ -23,16 +21,14 @@ defaults write com.apple.finder WarnOnEmptyTrash -bool false
 # Finder: show all filename extensions
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
-###############################################################################
-# Screen                                                                      #
+# Screen                                                                      
 ###############################################################################
 
 # Require password immediately after sleep or screen saver.
 defaults write com.apple.screensaver askForPassword -int 1
 defaults write com.apple.screensaver askForPasswordDelay -int 0
 
-###############################################################################
-# Dock, Dashboard                                                             #
+# Dock, Dashboard                                                             
 ###############################################################################
 
 # Minimize windows into their application’s icon
